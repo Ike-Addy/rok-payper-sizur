c65ccb04eac08aafb7d83c35937ed70de9e19644
Ike Addy <ikaddy05@gmail.com>
2023-11-03 08:33:27 -0400

    Changed getPlayerChoice func to a function express Was getting a syntax error msg so >>     Removed let declaration from playRound func Encountered a bug where vars in playRound returned undefined     Added return statements to the end of getplayer & getComp functions

--- scripts.js
+++ scripts.js
@@ -11,25 +11,31 @@ function getComputerChoice() {
         computerChoice = 'scissor';
         console.log(`The computer chose ${computerChoice}`);
     }
+    return computerChoice;
 }
 
-function getPlayerChoice() {
+const getPlayerChoice = function () {
     let playerChoice = prompt('Rock, Paper, or Scissor?').toLowerCase();
     if (playerChoice == 'rock') {
         playerChoice = 'rock';
         console.log(`You chose ${playerChoice}`);
     } else if (playerChoice == 'paper') {
-        playerChoice = 'rock';
+        playerChoice = 'paper';
+        console.log(`You chose ${playerChoice}`);
+    } else if (playerChoice == 'scissor'){
+        playerChoice = 'scissor';
         console.log(`You chose ${playerChoice}`);
     } else {
-        playerChoice = 'rock';
-        console.log(`You chose ${playerChoice}`);
+        console.log('Invalid choice. Please choose rock, paper, or scissor');
     }
+    return playerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
+   playerSelection = getPlayerChoice();
+//    console.log(playerSelection);
+   computerSelection = getComputerChoice();
+//    console.log(computerSelection);
 
     if (playerSelection == computerSelection) {
         console.log('its a tie!');
@@ -44,4 +50,8 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+playRound()
+playRound()
+playRound()
+playRound()
 playRound()
